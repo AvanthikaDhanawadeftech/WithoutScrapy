2fae91dabe679b5c7aba5f3237b4b2d9af741f9b
Chris Francis <chrisfrancis.dev@gmail.com>
2024-06-07 17:26:38 +0530

    Refactored the code and got the image URL

--- test.py
+++ test.py
@@ -2,171 +2,142 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from tqdm import tqdm
+import os
 
-list = []
-def fetch_article_details(article_url):
-    article_response = requests.get(article_url)
-    dictionary = {}
-    if article_response.status_code == 200:
-        article_soup = BeautifulSoup(article_response.content, 'html.parser')
-        h1_tag = article_soup.find('h1', class_='arttitle')
-        h2_tag = article_soup.find('h2', class_='artdec')
-        img_tag = article_soup.find('img', class_='lazy')
-        dictionary["URL"] = article_url
-        if h1_tag:
-            dictionary["Title"] = h1_tag.text.strip()
-            if h2_tag: 
-                dictionary["Summary"] = h2_tag.text.strip()     
-                if img_tag:
-                    img_src = img_tag['src']
-                    if img_src == "https://static.indiatvnews.com/ins-web/images/lazy-big.jpg":
-                        dictionary.update({"Image URL": "Unable to fetch"})
-                        dictionary["Image URL"] = "Unabel to fetch"
-                        pass
-                    else:
-                        dictionary.update({"Image URL": img_src})
-                        dictionary["Image URL"] = img_src
-                        pass
-                else:
-                    print("No images found")
-    else:
-        print(f'Failed to retrieve the article. Status code: {article_response.status_code}')
-        print("Article URL:", article_url)
-    list.append(dictionary)
-    
+parsed_results = []
+visited_articles = []
 
-def scrape_and_fetch(link):
+def parse_the_link(link):
+    if link in visited_articles:
+        return None
+    visited_articles.append(link)
     response = requests.get(link)
-
     if response.status_code == 200:
+        html_content = response.content
+        file_name = link.split('/')[-1] + '.html'  # Extract filename from URL
+        file_path = os.path.join('html_files', file_name)
+
+        # Create directory if it doesn't exist
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+        # Save HTML content to file
+        with open(file_path, 'wb') as file:
+            file.write(html_content)
         soup = BeautifulSoup(response.content, 'html.parser')
-        div_tag = soup.find('div', class_='row lhsBox sport_tnews mb20')
-
-        if div_tag:
-            thumb_links = div_tag.find_all('a', class_='thumb')
-
-            for thumb_link in thumb_links:
-                article_url = thumb_link.get('href')
-                fetch_article_details(article_url)
-        else:
-            print('Failed to find the div with class="row lhsBox sport_tnews mb20".')
+        return soup
     else:
         print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
         print("scrape_and_fetch", link)
+        return None
 
+def fetch_article_details(parsed_article):
+    article_soup = parse_the_link(parsed_article)
+    if article_soup == None:
+        return None
 
-def scrape_and_fetch2(link):
-    response = requests.get(link)
+    dictionary = {}
+    dictionary["URL"] = parsed_article
+    h1_tag = article_soup.find('h1', class_='arttitle')
+    h2_tag = article_soup.find('h2', class_='artdec')
+    img_tag = article_soup.find('img', class_='lazy')
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        div_tag = soup.find('div', class_='row lhsBox s_two_column pt20')
+    if h1_tag:
+        dictionary["Title"] = h1_tag.text.strip()
+        if h2_tag: 
+            dictionary["Summary"] = h2_tag.text.strip()     
+            if img_tag:
+                img_src = img_tag['data-original']
+                if img_src == "https://static.indiatvnews.com/ins-web/images/lazy-big.jpg":
+                    dictionary.update({"Image URL": "Unable to fetch"})
+                else:
+                    dictionary.update({"Image URL": img_src})
+            else:
+                print("No images found")   
+    parsed_results.append(dictionary)
 
-        if div_tag:
-            thumb_links = div_tag.find_all('a', class_='thumb')
-
-            for thumb_link in thumb_links:
-                article_url = thumb_link.get('href')
-                fetch_article_details(article_url)
-        else:
-            print('Failed to find the div with class="row lhsBox s_two_column pt20".')
+def scrape_and_fetch(link, div_class, soup):
+    div_tag = soup.find('div', class_=div_class)
+    if div_tag:
+        thumb_links = div_tag.find_all('a', class_='thumb')
+        for thumb_link in thumb_links:
+            parsed_article = thumb_link.get('href')
+            fetch_article_details(parsed_article)
     else:
-        print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
-        print("scrape_and_fetch2", link)
+        print(f'Failed to find the div with class={div_class}')
 
-
-def scrape_and_fetch3(link):
-    response = requests.get(link)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        li_tags = soup.find_all('li', class_='p_news eventTracking')
-
+def scrape_and_fetch4(link, soup):
+    div_tag1 = soup.find('div', class_='lhsBox top-news-list-election mb5 row')
+    if div_tag1:
+        big_news_div = div_tag1.find('div', class_='big-news eventTracking')
+        if big_news_div:
+            a_tag = big_news_div.find('a')
+            if a_tag:
+                parsed_article = a_tag.get('href')
+                fetch_article_details(parsed_article)
+    div_tag2 = soup.find('div', class_='big-news-list')
+    if div_tag2:
+        li_tags = div_tag2.find_all('li')
         for li_tag in li_tags:
-            thumb_links = li_tag.find_all('a', class_='thumb')
+            a_tag = li_tag.find('a')
+            if a_tag:
+                parsed_article = a_tag.get('href')
+                fetch_article_details(parsed_article)
 
-            for thumb_link in thumb_links:
-                article_url = thumb_link.get('href')
-                fetch_article_details(article_url)
+def scrape_and_fetch3(link, soup):
+    li_tags = soup.find_all('li', class_='p_news eventTracking')
+    for li_tag in li_tags:
+        thumb_links = li_tag.find_all('a', class_='thumb')
+        for thumb_link in thumb_links:
+            parsed_article = thumb_link.get('href')
+            fetch_article_details(parsed_article)
 
-        next_page_link = soup.find('a', class_='pnnext')
-
-        if next_page_link:
-            next_page_url = next_page_link.get('href')
-            scrape_and_fetch3(next_page_url)
-    else:
-        print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
-        print("scrape_and_fetch3", link)
-
-def scrape_and_fetch4(link):
-    response = requests.get(link)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # First part: div tag with class="lhsBox top-news-list-election mb5 row"
-        div_tag1 = soup.find('div', class_='lhsBox top-news-list-election mb5 row')
-        if div_tag1:
-            big_news_div = div_tag1.find('div', class_='big-news eventTracking')
-            if big_news_div:
-                a_tag = big_news_div.find('a')
-                if a_tag:
-                    article_url = a_tag.get('href')
-                    fetch_article_details(article_url)
-                
-        
-        # Second part: div tag with class="big-news-list"
-        div_tag2 = soup.find('div', class_='big-news-list')
-        if div_tag2:
-            li_tags = div_tag2.find_all('li')
-            for li_tag in li_tags:
-                a_tag = li_tag.find('a')
-                if a_tag:
-                    article_url = a_tag.get('href')
-                    fetch_article_details(article_url)            
-    else:
-        print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
-        print("scrape_and_fetch4", link)
+    next_page_link = soup.find('a', class_='pnnext')
+    if next_page_link:
+        next_page_url = next_page_link.get('href')
+        soup = parse_the_link(next_page_url)
+        if soup == None:
+            return None
+        scrape_and_fetch3(next_page_url, soup)
 
 def main():
-    url = 'https://www.indiatvnews.com/'
-    response = requests.get(url)
+    soup = parse_the_link('https://www.indiatvnews.com/')
+    if soup == None:
+        return None
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        ul_tag = soup.find('ul', class_='menu megamenu')
-        
-        links = []
+    ul_tag = soup.find('ul', class_='menu megamenu')
+    links = []
 
-        if ul_tag:
-            a_tags = ul_tag.find_all('a')
-            links = [a_tag.get('href') for a_tag in a_tags[2:-1] if a_tag.get('href')]
-        else:
-            print('Failed to find the ul with class="menu megamenu".')
-
-        # Define the indices of the links to use
-        indices_to_use = [8, 9, 10, 11, 12, 13, 15, 17, 22]
-        other_indices_to_use = [4, 7, 14, 18, 20, 21, 23, 25] 
-        # Iterate over the indices and use corresponding links
-        for index in tqdm(indices_to_use, desc="Processing Links"):
-            link = links[index]
-            scrape_and_fetch(link)
-            scrape_and_fetch2(link)
-            scrape_and_fetch3(link)
-            # scrape_and_fetch4(link)
-        print(len(list))
-        for index in tqdm(other_indices_to_use, desc="Processing Links"):
-            link = links[index]
-            scrape_and_fetch4(link)
-            scrape_and_fetch2(link)
-            scrape_and_fetch3(link)
-        
-        print(len(list))
-        with open("data2.json", "w") as json_file:
-        # Write the list to the JSON file
-            json.dump(list, json_file, indent=4)
+    if ul_tag:
+        a_tags = ul_tag.find_all('a')
+        links = [a_tag.get('href') for a_tag in a_tags[2:-1] if a_tag.get('href')]
     else:
-        print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
+        print('Failed to find the ul with class="menu megamenu".')
+
+    # print(len(links))
+    for link in tqdm(links, desc="Processing Links"):
+        # print(len(parsed_results))
+        soup = parse_the_link(link)
+        if soup == None:
+            continue
+        div1 = soup.find('div', class_='row lhsBox sport_tnews mb20')
+        div2 = soup.find('div', class_='row lhsBox s_two_column pt20')
+        div3 = soup.find('div', class_='lhsBox top-news-list-election mb5 row')
+        div4 = soup.find('div', class_='topNews row mb30')
+
+        if div1:
+            scrape_and_fetch(link, 'row lhsBox sport_tnews mb20', soup)
+        if div2:
+            scrape_and_fetch(link, 'row lhsBox s_two_column pt20', soup)
+        if div3:
+            scrape_and_fetch4(link, soup)
+        if div4:
+            scrape_and_fetch3(link, soup)
+
+    print(len(parsed_results))
+
+    with open("data.json", "w") as json_file:
+        json.dump(parsed_results, json_file, indent=4)
+    print("JSON file created successfully.")
 
 if __name__ == "__main__":
     main()
